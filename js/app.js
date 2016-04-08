f8a042716505f6592d8c875d342352164a5338f8
janvier byiringiro <janvier byiringiro>
2016-04-08 14:49:48 -0400

    user can change states now

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ var myApp = angular.module('myApp', []);
 
 							var appService = this;
             
-							appService.weather= function(zip) {
+							appService.weather= function(zip,state) {
 
-								var promise = $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + zip + '%2C%20va%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys').then(
+								var promise = $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + zip + '%2C%20'+ state +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys').then(
 												function(success) {
 													console.log(success.data);
 													return success.data;
@@ -24,10 +24,11 @@ var myApp = angular.module('myApp', []);
         myApp.controller('weatherCtrl', function($scope,AppService){
 
             $scope.zip= '22030';
+            $scope.state= 'VA';
             
             $scope.search = function() {
 
-				var promise = AppService.weather($scope.zip);
+				var promise = AppService.weather($scope.zip,$scope.state);
 
 				promise.then(function(data) {
 
